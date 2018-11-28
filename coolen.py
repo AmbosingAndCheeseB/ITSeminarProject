e984a6c2af3a82cbc7d2b8ce258208a03c254949
root <ambosing_@naver.com>
2018-11-29 00:07:11 +0900

    coolenjoy 링크 파싱

--- coolen.py
+++ coolen.py
@@ -3,7 +3,7 @@
 import requests
 from html.parser import HTMLParser
 
-class MLStripper(HTMLParser):
+class OurParser(HTMLParser):
     def __init__(self):
         self.reset()
         self.strict = False
@@ -13,11 +13,22 @@ class MLStripper(HTMLParser):
         self.fed.append(d)
     def get_data(self):
         return ''.join(self.fed)
+    def handle_starttag(self, tag, attrs):
+        if tag == "a":
+            for name, value in attrs:
+                if name == "href":
+                    print (name, "=", value)
 
 def strip_tags(html):
-    s = MLStripper()
+    s = OurParser()
     s.feed(html)
     return s.get_data()
+
+def only_href(html):
+    s = OurParser()
+    s.feed(html)
+    s.handle_starttag()
+
 
 
 
@@ -26,3 +37,5 @@ url = "http://www.coolenjoy.net/bbs/jirum/p1"
 html_result = requests.get(url)
 
 print(strip_tags(html_result.text))
+
+only_href(html_result.text)
