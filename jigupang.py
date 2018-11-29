cb6b30d4c7bcad37787d9569d09b450d9ab32228
CheeseB <2489ckckck@naver.com>
2018-11-29 23:09:38 +0900

    practice

--- jigupang.py
+++ jigupang.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 
 import requests
+from html.parser import HTMLParser
+
+class MyParser(HTMLParser):
+    def handle_starttag(self,tag,attrs):
+        print("start tag:",tag)
+    
+    def handle_endtag(self,tag):
+        print("end tag:",tag)
+
+    def handle_data(self, data):
+        print("some data:",data)
+
+parser = MyParser()
 
 url = "https://www.pangdeals.com"
 response = requests.get(url)
-text = response.text
-print(text)
+
+parser.feed(response.text)
+parser.handle_starttag()
+parser.handle_endtag()
+parser.handle_data()
