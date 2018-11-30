a9eababc97bd0ae11f45071c7c26fa45e03e70b0
CheeseB <2489ckckck@naver.com>
2018-12-01 00:00:02 +0900

    modify

--- jigupang.py
+++ jigupang.py
@@ -5,14 +5,15 @@ from html.parser import HTMLParser
 
 class MyParser(HTMLParser):
     
-    def handle_starttag(self, tag, attrs):
-        print("start tag:",tag)
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.fed = []
 
-    def handle_endtag(self, tag):
-        print("end tag:",tag)
 
     def handle_data(self, data):
-       print("some data:",data)
+        print("data:",data)
 
 
 url = "https://www.pangdeals.com"
