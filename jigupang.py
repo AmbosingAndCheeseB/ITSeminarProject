56e13a41385bd21ddcec8d6538b0dbb10f7b79bc
CheeseB <2489ckckck@naver.com>
2018-12-01 01:31:46 +0900

    modify

--- jigupang.py
+++ jigupang.py
@@ -13,23 +13,11 @@ class MyParser(HTMLParser):
         self.fed.append(d)
     def get_data(self):
         return ''.join(self.fed)
-    def handle_starttag(self, tag, attrs):
-        if tag == "a":
-            for name, value in attrs:
-                if name == "href":
-                    print (name, "=", value)
 
 def strip_tags(html):
-    s = MyParser()
-    s.feed(html)
-    return s.get_data()
-
-def only_href(html):
-    s = MyParser()
-    s.feed(html)
-    s.handle_starttag()
-
-
+    parser = MyParser()
+    parser.feed(html)
+    return parser.get_data()
 
 
 url = "https://www.pangdeals.com"
@@ -37,5 +25,3 @@ url = "https://www.pangdeals.com"
 response = requests.get(url)
 
 print(strip_tags(response.text))
-
-only_href(response.text)
