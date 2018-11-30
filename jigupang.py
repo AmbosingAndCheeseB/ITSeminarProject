c378a3727af8996157d9b696874d9601a55faeaf
CheeseB <2489ckckck@naver.com>
2018-12-01 01:05:06 +0900

    modify

--- jigupang.py
+++ jigupang.py
@@ -4,20 +4,38 @@ import requests
 from html.parser import HTMLParser
 
 class MyParser(HTMLParser):
-    
     def __init__(self):
         self.reset()
         self.strict = False
         self.convert_charrefs = True
         self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+    def handle_starttag(self, tag, attrs):
+        if tag == "a":
+            for name, value in attrs:
+                if name == "href":
+                    print (name, "=", value)
+
+def strip_tags(html):
+    s = MyParser()
+    s.feed(html)
+    return s.get_data()
+
+def only_href(html):
+    s = MyParser()
+    s.feed(html)
+    s.handle_starttag()
 
 
-    def handle_data(self, data):
-        print("data:",data)
 
 
 url = "https://www.pangdeals.com"
+
 response = requests.get(url)
 
-parser = MyParser()
-parser.feed(response.text)
+print(strip_tags(response.text))
+
+only_href(response.text)
