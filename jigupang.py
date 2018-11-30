bc18303b3f333e4c60d475ada60a3930b0204053
CheeseB <2489ckckck@naver.com>
2018-11-30 22:04:46 +0900

    modify

--- jigupang.py
+++ jigupang.py
@@ -4,21 +4,13 @@ import requests
 from html.parser import HTMLParser
 
 class MyParser(HTMLParser):
-    def handle_starttag(self,tag,attrs):
-        print("start tag:",tag)
-    
-    def handle_endtag(self,tag):
-        print("end tag:",tag)
 
     def handle_data(self, data):
         print("some data:",data)
 
-parser = MyParser()
 
 url = "https://www.pangdeals.com"
 response = requests.get(url)
 
+parser = MyParser()
 parser.feed(response.text)
-parser.handle_starttag()
-parser.handle_endtag()
-parser.handle_data()
