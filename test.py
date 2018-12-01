09ba33ee42c14fdb50f013250a0c67e884892e22
CheeseBall <2489ckckck@naver.com>
2018-12-01 23:08:52 +0900

    modify

--- test.py
+++ test.py
@@ -3,21 +3,23 @@
 import requests
 from html.parser import HTMLParser
 
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self,tag,attrs):
-        print("start tag: ",tag)
 
-    def handle_endtag(self,tag):
-        print("end tag: ",tag)
+class MyParser(HTMLParser):
 
-    def handle_data(self,data):
-        print("data: ",data)
+    def handle_starttag(self, tag, attrs):
+        print("start tag: ", tag)
 
-parser = MyHTMLParser()
-parser.feed('<html><head><title>test</title></head></html>')
+    def handle_endtag(self, tag):
+        print("end tag: ", tag)
 
-#url = "https://www.pangdeals.com"
-#response = requests.get(url)
+    def handle_data(self, data):
+        print("data: ", data)
 
-#parser=MyHTMLParser()
-#parser.feed(response.text)
+
+parser = MyParser()
+
+url = "https://www.pangdeals.com"
+response = requests.get(url)
+
+parser = MyParser()
+parser.feed(response.text)
