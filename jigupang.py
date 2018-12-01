0a284b8cc41a8452473ed17e67debdbe2d9fa180
CheeseBall <2489ckckck@naver.com>
2018-12-01 23:14:41 +0900

    modify

--- jigupang.py
+++ jigupang.py
@@ -3,19 +3,22 @@
 import requests
 from html.parser import HTMLParser
 
+
 class MyParser(HTMLParser):
+
     def __init__(self):
         self.reset()
-        self.strict = False
-        self.convert_charrefs = True
         self.fed = []
 
-#    def handle_starttag(self,tag,attr):
-#        return tag
+#    def handle_starttag(self, tag, attrs):
+#        print("start tag: ", tag)
+
+#    def handle_endtag(self, tag):
+#        print("end tag: ", tag)
 
     def handle_data(self, data):
         self.fed.append(data)
-        
+
     def get_data(self):
         return ''.join(self.fed)
 
