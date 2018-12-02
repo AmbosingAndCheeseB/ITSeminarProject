7b2597a92fce9664dca0c2e15b42f4e928626880
yangjiwon <ambosing_@naver.com>
2018-12-02 16:12:38 +0900

    쿨엔조이 링크 파싱

--- coolen.py
+++ coolen.py
@@ -4,38 +4,48 @@ import requests
 from html.parser import HTMLParser
 
 class OurParser(HTMLParser):
+
     def __init__(self):
         self.reset()
         self.strict = False
         self.convert_charrefs = True
-        self.fed = []
-    def handle_data(self, d):
-        self.fed.append(d)
-    def get_data(self):
-        return ''.join(self.fed)
+        self.link = []
+
     def handle_starttag(self, tag, attrs):
-        if tag == "a":
-            for name, value in attrs:
-                if name == "href":
-                    print (name, "=", value)
+                if tag == "a":
+                    for name, value in attrs:
+                        if name == "href":
+                            self.link.append(value)
+    def get_link(self):
+        return self.link
 
-def strip_tags(html):
-    s = OurParser()
+pass
+
+
+def only_href(s, html):
     s.feed(html)
-    return s.get_data()
+    try:
 
-def only_href(html):
-    s = OurParser()
-    s.feed(html)
-    s.handle_starttag()
+        s.handle_starttag()
+    except:
+        return
+
+def need_href(s):
+    temp = s.get_link()
+    new_link = []
+    for item in temp:
+        if '/jirum/' in item and '?sca=PC' in item:
+            new_link.append(item)
+    return new_link
 
 
-
-
-url = "http://www.coolenjoy.net/bbs/jirum/p1"
+url = "http://www.coolenjoy.net/bbs/jirum?sca=PC%EA%B4%80%EB%A0%A8"
 
 html_result = requests.get(url)
+s = OurParser()
 
-print(strip_tags(html_result.text))
+only_href(s, html_result.text)
 
-only_href(html_result.text)
+print(s.get_link())
+
+print(need_href(s))
