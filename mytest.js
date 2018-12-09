05a82695a3c3573e4ad776b5e500e4553b2275a1
CheeseBall <2489ckckck@naver.com>
2018-12-09 19:51:55 +0900

    test

--- mytest.js
+++ mytest.js
@@ -1,49 +1,12 @@
-//TODO: Make es6 class, this is a quick example
-
-var events = document.querySelectorAll('.event');
-var eventsWrapper = document.querySelector('.events-wrapper');
-var eventsViewPort = document.querySelector('.events-viewport');
-var eventsLoadMore = document.querySelector('.events-load-more');
-var eventsChunks = _.chunk(events, 9);
-var eventLandmarks = getLandmarks(eventsChunks);
-var eventLandmarkIndex = 0;
-var eventLandmarkIndexMax = eventLandmarks.length - 1;
-
-function getLandmarks($chunks) {
-  var landmarks = [];
-  for(var $i = 0; $i < $chunks.length; $i++) {
-    var chunk = $chunks[$i];
-    var landmark = chunk[chunk.length - 1];
-    landmarks.push(landmark.offsetTop + landmark.offsetHeight);
-  }
-  return landmarks;
-}
-
-function updateViewportHeight(landmark) {
-  eventsViewPort.style.height = landmark + 'px';
-}
-
-// Run on initialize
-function init() {
-  updateViewportHeight(eventLandmarks[eventLandmarkIndex]);
-}
-
-
-function onResize() {
-  eventLandmarks = getLandmarks(eventsChunks); // re-fetch landmark locations
-  init();
-}
-
-init();
-
-eventsLoadMore.addEventListener('click', (e) => {
-  eventLandmarkIndex++;
-  if(eventLandmarkIndex === eventLandmarkIndexMax) {
-    eventsLoadMore.style.display = 'none';
-    eventsViewPort.style.height = 'auto';
-  } else {                
-    updateViewportHeight(eventLandmarks[eventLandmarkIndex]);
-  }
+$(document).ready(function(){
+	"use strict";
+  $(".content").slice(0, 4).show();
+  $("#loadMore").on("click", function(e){
+    e.preventDefault();
+    $(".content:hidden").slice(0, 4).slideDown();
+    if($(".content:hidden").length === 0) {
+      $("#loadMore").text("No Content").addClass("noContent");
+    }
+  });
+  
 });
-
-window.addEventListener('resize', _.debounce(onResize, 200))
